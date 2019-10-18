835a267db04a2849bf9d23d8d36acd38087cb82e
Timothy Sharkey <Techne3@gmail.com>
2019-10-18 09:32:05 -0600

    Set up Login page and axiosWithAuth Set up PrivatRoute

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,56 @@
-import React from "react";
+import React,{useState} from "react";
+import axiosWithAuth from '../utils/axiosWithAuth'
+import axios from 'axios'
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: '', 
+});
+
+  const changeHandle = e => {
+    setCredentials({
+      ...credentials, 
+      [e.target.name]:e.target.value,
+    })
+  }
+
+  const submitHandle = e => {
+    e.preventDefault();
+    axiosWithAuth()
+    .post(`http://localhost:5000/api/login`, credentials)
+    .then(res => {
+      // console.log(res.data)
+      localStorage.setItem('token', res.data.payload);
+    })
+    .catch(err => console.log(err.response))
+  }
+
+
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className="loginForm">
+        <form onSubmit={submitHandle}>
+            <input 
+            type='text'
+            name='username'
+            placeholder='username'  
+            value={credentials.username}
+            onChange={changeHandle}
+            />
+            <input 
+            type='password'
+            name='password'
+            placeholder='password'  
+            value={credentials.password}
+            onChange={changeHandle}
+            />
+            <button type='submit'>Login</button>
+        </form>
+        </div>
   );
 };
 
